5bc4977d662afb55698f2f8a5ccb38d55b1417ef
Antoine Tran <antoine2001.tran@gmail.com>
2020-04-29 02:54:48 +0200

    removed useless code that was just there for testing

--- core/src/test/java/net/akami/yggdrasil/util/SpaceScannerTest.java
+++ core/src/test/java/net/akami/yggdrasil/util/SpaceScannerTest.java
@@ -3,13 +3,6 @@ package net.akami.yggdrasil.util;
 import com.flowpowered.math.vector.Vector3d;
 import net.akami.yggdrasil.api.utils.SpaceScanner;
 import org.junit.Test;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class SpaceScannerTest {
 
@@ -24,44 +17,7 @@ public class SpaceScannerTest {
                 .shift(SpaceScanner.asShift(center))
                 .withAction(System.out::println)
                 .build();
-
-        combineDimensions(Flux.just(1, 2, 3), Flux.just(1, 2, 3), Flux.just(1, 2, 3));
-
-    }
-
-    public <R> void combineDimensions(Flux<R>... layers) {
-        Flux<List<R>> combined = Flux.from(Mono.just(new ArrayList<>()));
-
-        for (Flux<R> additionalLayer : layers) {
-            combined = combined.flatMap(list -> {
-                Flux<List<R>> map = additionalLayer.map(x ->
-                {
-                    List<R> values = new ArrayList<>(list);
-                    values.add(x);
-                    return values;
-                });
-                return map;
-            });
-            System.out.println(combined.toStream().collect(Collectors.toList()));
-            System.out.println("...");
-        }
-    }
-
-    class MyTest {
-        List<String> list = Arrays.asList("a", "b", "c");
-        int n;
-
-        MyTest(int n) {
-            this.n = n;
-        }
-
-        public List<String> getList() {
-            return list;
-        }
-
-        @Override
-        public String toString() {
-            return "" + n;
-        }
+        scanner.run();
     }
 }
+
